8bb7f1e7d0ed4e3e80847fd1b4c030a310761dc7
Maria Teresa <grippomariateresa@gmail.com>
2017-09-29 14:45:16 +0200

    - Core/include/AnalysisMath.h: fixed crystalBall function with constatns   from boost

--- Core/include/AnalysisMath.h
+++ Core/include/AnalysisMath.h
@@ -16,6 +16,8 @@ This file is part of https://github.com/hh-italian-group/AnalysisTools. */
 #include <TH2.h>
 #include <TMatrixD.h>
 #include <TLorentzVector.h>
+#include <boost/math/constants/constants.hpp>
+#include <boost/math/special_functions/erf.hpp>
 
 
 #include "PhysicalValue.h"
@@ -256,36 +258,38 @@ inline void RenormalizeHistogram(Histogram& histogram, double norm, bool include
     histogram.Scale(norm / integral);
 }
 
-inline double crystalballEfficiency(double m, double m0, double sigma, double alpha, double n, double norm)
+inline double crystalball(double m, double m0, double sigma, double alpha, double n, double norm)
 {
-    if(m<1. || 1000.<m) return 0.; //safety check
+    if(m<1. || 1000.<m)
+        throw analysis::exception("The parameter m is out of range");
 
-    const double sqrtPiOver2 = 1.2533141373;
-    const double sqrt2 = 1.4142135624;
-    double sig = fabs((double) sigma);
+    static constexpr double sqrtPiOver2 = boost::math::constants::root_half_pi<double>();
+    static constexpr double sqrt2 = boost::math::constants::root_two<double>();
+    double sig = std::abs(sigma);
     double t = (m - m0)/sig;
     if(alpha < 0) t = -t;
-    double absAlpha = fabs(alpha/sig);
-    double a = TMath::Power(n/absAlpha,n)*exp(-0.5*absAlpha*absAlpha);
+    double absAlpha =  std::abs(alpha/sig);
+    double a = std::pow(n/absAlpha,n)*std::exp(-0.5*absAlpha*absAlpha);
     double b = absAlpha - n/absAlpha;
     double ApproxErf;
     double arg = absAlpha / sqrt2;
     if (arg > 5.) ApproxErf = 1;
     else if (arg < -5.) ApproxErf = -1;
-    else ApproxErf = TMath::Erf(arg);
+    else ApproxErf = boost::math::erf(arg);
     double leftArea = (1 + ApproxErf) * sqrtPiOver2;
-    double rightArea = ( a * 1/TMath::Power(absAlpha - b,n-1)) / (n - 1);
+    double rightArea = ( a * 1/std::pow(absAlpha - b,n-1)) / (n - 1);
     double area = leftArea + rightArea;
     if( t <= absAlpha ) {
         arg = t / sqrt2;
         if(arg > 5.) ApproxErf = 1;
         else if (arg < -5.) ApproxErf = -1;
-        else ApproxErf = TMath::Erf(arg);
+        else ApproxErf = boost::math::erf(arg);
         return norm * (1 + ApproxErf) * sqrtPiOver2 / area;
     }
     else {
-        return norm * (leftArea + a * (1/TMath::Power(t-b,n-1) -  1/TMath::Power(absAlpha - b,n-1)) / (1 - n)) / area;
+        return norm * (leftArea + a * (1/std::pow(t-b,n-1) -  1/std::pow(absAlpha - b,n-1)) / (1 - n)) / area;
     }
 }
+
 
 } // namespace analysis
